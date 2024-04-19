258a40bbf885c2f3479a4110777cb0a2dea0406c
PabloJL <jup_jim_7@hotmail.com>
2024-04-19 15:08:18 -0600

    Home page and book pages created

---
+++ src/data/books.js
@@ -0,0 +1,34 @@
+export const books = [
+  {
+    id: "dune-1",
+    title: "Dune",
+    author: "Frank Herbert",
+    img: "https://m.media-amazon.com/images/I/A1u+2fY5yTL._AC_UF1000,1000_QL80_.jpg",
+    opinion:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum amet officiis,velit soluta voluptates debitis, aliquid ut exercitationem dolor dolorem tempora. Expedita sequi dolore, praesentium eaque quod corrupti totam enim?",
+  },
+  {
+    id: "meditaciones",
+    title: "Meditaciones de Marco Aurelio",
+    author: "",
+    img: "https://m.media-amazon.com/images/I/61ably5-mrL._AC_UF1000,1000_QL80_.jpg",
+    opinion:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum amet officiis,velit soluta voluptates debitis, aliquid ut exercitationem dolor dolorem tempora. Expedita sequi dolore, praesentium eaque quod corrupti totam enim?",
+  },
+  {
+    id: "cien-años-de-soledad",
+    title: "Cien años de soledad",
+    author: "",
+    img: "https://m.media-amazon.com/images/I/A1lNJP8sC6L._AC_UF1000,1000_QL80_.jpg",
+    opinion:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum amet officiis,velit soluta voluptates debitis, aliquid ut exercitationem dolor dolorem tempora. Expedita sequi dolore, praesentium eaque quod corrupti totam enim?",
+  },
+  {
+    id: "lonesome-dove",
+    title: "Lonesome Dove",
+    author: "",
+    img: "https://m.media-amazon.com/images/I/71JXquqq54L._AC_UF1000,1000_QL80_.jpg",
+    opinion:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum amet officiis,velit soluta voluptates debitis, aliquid ut exercitationem dolor dolorem tempora. Expedita sequi dolore, praesentium eaque quod corrupti totam enim?",
+  },
+];
