1c341a66035efb1e74beb1000ec69785ca8d5ef2
PabloJL <jup_jim_7@hotmail.com>
2024-04-22 16:29:59 -0600

    Changes

--- src/data/books.js
+++ src/data/books.js
@@ -4,30 +4,34 @@ export const books = [
     title: "Dune",
     author: "Frank Herbert",
     img: "https://m.media-amazon.com/images/I/A1u+2fY5yTL._AC_UF1000,1000_QL80_.jpg",
+    pages: "900",
     opinion:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum amet officiis,velit soluta voluptates debitis, aliquid ut exercitationem dolor dolorem tempora. Expedita sequi dolore, praesentium eaque quod corrupti totam enim?",
   },
   {
     id: "meditaciones",
     title: "Meditaciones de Marco Aurelio",
-    author: "",
+    author: "Marco Auerlio",
     img: "https://m.media-amazon.com/images/I/61ably5-mrL._AC_UF1000,1000_QL80_.jpg",
+    pages: "120",
     opinion:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum amet officiis,velit soluta voluptates debitis, aliquid ut exercitationem dolor dolorem tempora. Expedita sequi dolore, praesentium eaque quod corrupti totam enim?",
   },
   {
     id: "cien-años-de-soledad",
     title: "Cien años de soledad",
-    author: "",
+    author: "Gabriel Garcia Marquez",
     img: "https://m.media-amazon.com/images/I/A1lNJP8sC6L._AC_UF1000,1000_QL80_.jpg",
+    pages: "500",
     opinion:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum amet officiis,velit soluta voluptates debitis, aliquid ut exercitationem dolor dolorem tempora. Expedita sequi dolore, praesentium eaque quod corrupti totam enim?",
   },
   {
     id: "lonesome-dove",
     title: "Lonesome Dove",
-    author: "",
+    author: "Larry McMurtry",
     img: "https://m.media-amazon.com/images/I/71JXquqq54L._AC_UF1000,1000_QL80_.jpg",
+    pages: "890",
     opinion:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum amet officiis,velit soluta voluptates debitis, aliquid ut exercitationem dolor dolorem tempora. Expedita sequi dolore, praesentium eaque quod corrupti totam enim?",
   },
