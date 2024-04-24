3b8fc48e1026d78d0fb62f6c4210849fbc5bcfeb
PabloJL <jup_jim_7@hotmail.com>
2024-04-23 18:04:47 -0600

    Modal UI finished

--- src/components/Comments/AddComment.jsx
+++ src/components/Comments/AddComment.jsx
@@ -30,7 +30,7 @@ export const AddComment = () => {
   };
 
   return (
-    <div>
+    <div className="py-4">
       <button
         onClick={handleModalToggle}
         className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -38,21 +38,21 @@ export const AddComment = () => {
         data-modal-target="crud-modal"
         data-modal-toggle="crud-modal"
       >
-        Toggle modal
+        Write Review
       </button>
 
       {isModalOpen && (
         <div
           id="crud-modal"
-          tabIndex="-1"
+          tabindex="-1"
           aria-hidden="true"
-          className="fixed top-0 right-0 left-0 z-50 justify-center items-center w-full h-screen bg-black bg-opacity-50 overflow-y-auto overflow-x-hidden"
+          class="flex flex-col overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
         >
           <div className="relative p-4 w-full max-w-md">
-            <div className="bg-white rounded-lg shadow">
+            <div className="bg-white rounded-lg shadow dark:bg-zinc-900">
               <div className="flex items-center justify-between p-4 border-b rounded-t">
                 <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                  Create New Product
+                  Write your opinion
                 </h3>
                 <button
                   onClick={handleModalToggle}
@@ -83,7 +83,7 @@ export const AddComment = () => {
                       htmlFor="name"
                       className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                     >
-                      Name
+                      Title
                     </label>
                     <input
                       type="text"
@@ -91,17 +91,17 @@ export const AddComment = () => {
                       name="name"
                       value={formData.name}
                       onChange={handleChange}
-                      placeholder="Type product name"
+                      placeholder="Give your opinion in a few words..."
                       required
                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                     />
                   </div>
-                  <div className="col-span-2 sm:col-span-1">
+                  <div className="col-span-2 sm:col-span-2">
                     <label
                       htmlFor="price"
                       className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                     >
-                      Price
+                      Rating
                     </label>
                     <input
                       type="number"
@@ -109,39 +109,18 @@ export const AddComment = () => {
                       name="price"
                       value={formData.price}
                       onChange={handleChange}
-                      placeholder="$2999"
+                      placeholder="--/10"
                       required
                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                     />
                   </div>
-                  <div className="col-span-2 sm:col-span-1">
-                    <label
-                      htmlFor="category"
-                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                    >
-                      Category
-                    </label>
-                    <select
-                      id="category"
-                      name="category"
-                      value={formData.category}
-                      onChange={handleChange}
-                      required
-                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                    >
-                      <option value="">Select category</option>
-                      <option value="TV">TV/Monitors</option>
-                      <option value="PC">PC</option>
-                      <option value="GA">Gaming/Console</option>
-                      <option value="PH">Phones</option>
-                    </select>
-                  </div>
+
                   <div className="col-span-2">
                     <label
                       htmlFor="description"
                       className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                     >
-                      Product Description
+                      What are your thoughts on this book?
                     </label>
                     <textarea
                       id="description"
@@ -149,7 +128,7 @@ export const AddComment = () => {
                       value={formData.description}
                       onChange={handleChange}
                       rows="4"
-                      placeholder="Write product description here"
+                      placeholder="Whad did you liked? what didn't you liked?"
                       className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     ></textarea>
                   </div>
@@ -159,18 +138,26 @@ export const AddComment = () => {
                   className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
                   <svg
-                    className="me-1 -ms-1 w-5 h-5"
+                    class="me-1 -ms-1 w-5 h-5 "
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
                     fill="currentColor"
                     viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
                   >
                     <path
-                      fillRule="evenodd"
-                      d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-                      clipRule="evenodd"
-                    ></path>
+                      fill-rule="evenodd"
+                      d="M8 7V2.221a2 2 0 0 0-.5.365L3.586 6.5a2 2 0 0 0-.365.5H8Zm2 0V2h7a2 2 0 0 1 2 2v.126a5.087 5.087 0 0 0-4.74 1.368v.001l-6.642 6.642a3 3 0 0 0-.82 1.532l-.74 3.692a3 3 0 0 0 3.53 3.53l3.694-.738a3 3 0 0 0 1.532-.82L19 15.149V20a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V9h5a2 2 0 0 0 2-2Z"
+                      clip-rule="evenodd"
+                    />
+                    <path
+                      fill-rule="evenodd"
+                      d="M17.447 8.08a1.087 1.087 0 0 1 1.187.238l.002.001a1.088 1.088 0 0 1 0 1.539l-.377.377-1.54-1.542.373-.374.002-.001c.1-.102.22-.182.353-.237Zm-2.143 2.027-4.644 4.644-.385 1.924 1.925-.385 4.644-4.642-1.54-1.54Zm2.56-4.11a3.087 3.087 0 0 0-2.187.909l-6.645 6.645a1 1 0 0 0-.274.51l-.739 3.693a1 1 0 0 0 1.177 1.176l3.693-.738a1 1 0 0 0 .51-.274l6.65-6.646a3.088 3.088 0 0 0-2.185-5.275Z"
+                      clip-rule="evenodd"
+                    />
                   </svg>
-                  Add new product
+                  Post Review
                 </button>
               </form>
             </div>
