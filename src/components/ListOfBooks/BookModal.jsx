d92e4903040b5ebefb6b554b6e03ad9842a41584
PabloJL <jup_jim_7@hotmail.com>
2024-04-26 20:00:19 -0600

    Syles added to modal

--- src/components/ListOfBooks/BookModal.jsx
+++ src/components/ListOfBooks/BookModal.jsx
@@ -6,30 +6,49 @@ export default function BookModal({ title, img, handleClick }) {
       className="flex flex-col overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
     >
       <div className="relative p-4  w-full h-3/4 flex justify-center">
-        <div className="flex flex-col w-full items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-3xl  dark:border-zinc-800 dark:bg-zinc-900 ">
-          <img
-            className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
-            src={img}
-            alt={`Cover of book ${title}`}
-          />
-          <aside className="flex flex-col justify-between p-4 leading-normal w-full">
-            <div className="text-left">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                {title}
-              </h5>
-
-              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                Started on: 25-04-2024
-              </p>
-            </div>
-
+        <div className="flex flex-col w-full h-full items-center bg-white border border-gray-200 rounded-lg shadow md:max-w-3xl  dark:border-zinc-800 dark:bg-zinc-900 ">
+          <div class="flex items-center justify-between p-4 md:p-5 rounded-t w-full">
+            <h3 class="text-2xl font-semibold text-gray-900 dark:text-white">
+              Edit Progress
+            </h3>
             <button
-              className=" w-18 h-18 bg-red-600 rounded-xl mt-8 p-3 text-lg text-white font-bold uppercase"
               onClick={handleClick}
+              type="button"
+              class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+              data-modal-hide="default-modal"
             >
-              Close
+              <svg
+                class="w-3 h-3"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 14"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                />
+              </svg>
+              <span class="sr-only">Close modal</span>
             </button>
-          </aside>
+          </div>
+          <section className="flex flex-col md:flex-row md:h-full md:w-full md:gap-10 ">
+            <picture className="md:h-full md:w-1/2 p-4 h-1/2 w-1/2">
+              <img
+                src={img}
+                alt={`Cover of book ${title}`}
+                className="rounded-xl"
+              />
+            </picture>
+            <aside className="p-4">
+              <h1 className="dark:text-white md:w-1/2 text-2xl font-bold ">
+                {title}
+              </h1>
+            </aside>
+          </section>
         </div>
       </div>
     </div>
