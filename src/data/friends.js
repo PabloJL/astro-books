20a1d0692e0b9620bf8f25ac385775a22335a7a0
PabloJL <jup_jim_7@hotmail.com>
2024-04-29 19:01:10 -0600

    Friends page modified

---
+++ src/data/friends.js
@@ -0,0 +1,27 @@
+export const friends = [
+  {
+    name: "Neil Sims",
+    img: "https://flowbite.com/docs/images/people/profile-picture-1.jpg",
+    status: "Offline",
+  },
+  {
+    name: "Bonnie Green",
+    img: "https://flowbite.com/docs/images/people/profile-picture-3.jpg",
+    status: "Online",
+  },
+  {
+    name: "Michael Gough",
+    img: "https://flowbite.com/docs/images/people/profile-picture-2.jpg",
+    status: "Online",
+  },
+  {
+    name: "Lana Byrd",
+    img: "https://flowbite.com/docs/images/people/profile-picture-4.jpg",
+    status: "OffLine",
+  },
+  {
+    name: "Thomes Lean",
+    img: "https://flowbite.com/docs/images/people/profile-picture-5.jpg",
+    status: "Online",
+  },
+];
