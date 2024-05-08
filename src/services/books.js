096fdf8fbbd64d83e9786f5607c6a33b38fb1bca
PabloJL <jup_jim_7@hotmail.com>
2024-05-08 17:09:08 -0600

    changes

--- src/services/books.js
+++ src/services/books.js
@@ -1,7 +1,5 @@
 export const getBooks = async () => {
-  const res = await fetch(
-    "https://www.googleapis.com/books/v1/volumes?q=dune&key=AIzaSyAgNUguzdGrqdqyZ3yvYW6QvGklSk-4kww"
-  );
+  const res = await fetch();
 
   const books = await res.json();
   return { books };
