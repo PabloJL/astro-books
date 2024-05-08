8d4088b88c78db6ba4ede7e2353540ba8987384b
PabloJL <jup_jim_7@hotmail.com>
2024-05-07 18:23:00 -0600

    Changes to login page

--- src/components/ProfileToggle.jsx
+++ src/components/ProfileToggle.jsx
@@ -52,7 +52,7 @@ export const ProfileToggle = () => {
 
           <li>
             <a
-              href="#"
+              href="/logIn"
               className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
             >
               Sign out
