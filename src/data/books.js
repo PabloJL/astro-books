56c003f5807b767f3eda9905cbc872b488b2bbc3
PabloJL <jup_jim_7@hotmail.com>
2024-05-09 15:15:49 -0600

    books info updated

--- src/data/books.js
+++ src/data/books.js
@@ -6,7 +6,7 @@ export const books = [
     img: "https://m.media-amazon.com/images/I/A1u+2fY5yTL._AC_UF1000,1000_QL80_.jpg",
     pages: "900",
     opinion:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum amet officiis,velit soluta voluptates debitis, aliquid ut exercitationem dolor dolorem tempora. Expedita sequi dolore, praesentium eaque quod corrupti totam enim?",
+      "It tells the story of young Paul Atreides, whose family accepts the stewardship of the planet Arrakis. While the planet is an inhospitable and sparsely populated desert wasteland, it is the only source of melange, or spice, a drug that extends life and enhances mental abilities.",
   },
   {
     id: "meditaciones",
@@ -15,7 +15,7 @@ export const books = [
     img: "https://m.media-amazon.com/images/I/61ably5-mrL._AC_UF1000,1000_QL80_.jpg",
     pages: "120",
     opinion:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum amet officiis,velit soluta voluptates debitis, aliquid ut exercitationem dolor dolorem tempora. Expedita sequi dolore, praesentium eaque quod corrupti totam enim?",
+      "What is Meditations about? Meditations (170-180 AD) is a journey through the mind of the great Roman emperor, Marcus Aurelius. These blinks offer philosophical ruminations on the meaning of death and justice, the nature of the world and why things happen the way they do.",
   },
   {
     id: "cien-años-de-soledad",
@@ -24,7 +24,7 @@ export const books = [
     img: "https://m.media-amazon.com/images/I/A1lNJP8sC6L._AC_UF1000,1000_QL80_.jpg",
     pages: "500",
     opinion:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum amet officiis,velit soluta voluptates debitis, aliquid ut exercitationem dolor dolorem tempora. Expedita sequi dolore, praesentium eaque quod corrupti totam enim?",
+      "One Hundred Years of Solitude is a captivating literary masterpiece by Gabriel García Márquez. It tells the story of the Buendía family across generations, revealing the cyclical nature of life and the profound impact of solitude on human existence.",
   },
   {
     id: "lonesome-dove",
@@ -33,6 +33,6 @@ export const books = [
     img: "https://m.media-amazon.com/images/I/71JXquqq54L._AC_UF1000,1000_QL80_.jpg",
     pages: "890",
     opinion:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum amet officiis,velit soluta voluptates debitis, aliquid ut exercitationem dolor dolorem tempora. Expedita sequi dolore, praesentium eaque quod corrupti totam enim?",
+      "The novel, set in the waning days of the Old West centers around the relationships between several retired Texas Rangers and their adventures driving a cattle herd from Texas to Montana. The novel contains themes including old age, death, unrequited love, and friendship",
   },
 ];
