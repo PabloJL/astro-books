333c8fb65bf610569f5be6d0cf090a34aa433831
PabloJL <jup_jim_7@hotmail.com>
2024-06-06 12:52:17 -0600

    Bottom navigation created for smaller screens

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -9,11 +9,10 @@ export default function Search() {
 
   return (
     <div className="w-full h-full">
-      <div class="hidden md:block relative">
-        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-          <button></button>
+      <div className="hidden md:block relative">
+        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
           <svg
-            class="w-4 h-4 text-gray-500 dark:text-gray-400"
+            className="w-4 h-4 text-gray-500 dark:text-gray-400"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -31,9 +30,37 @@ export default function Search() {
         <input
           type="search"
           id="default-search"
-          class="block  w-full h-1/4 p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-zinc-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-zinc-700 dark:border-zinc-600 dark:placeholder-zinc-400 dark:text-white dark:focus:ring-zinc-500 dark:focus:border-zinc-500"
+          className="block  w-full h-1/4 p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-zinc-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-zinc-700 dark:border-zinc-600 dark:placeholder-zinc-400 dark:text-white dark:focus:ring-zinc-500 dark:focus:border-zinc-500"
           placeholder="Search books..."
         />
+      </div>
+
+      <div className="relative md:hidden">
+        <button className="w-8 h-8 " onClick={handleToggle}>
+          <svg
+            className="w-8 h-8 text-white dark:text-white"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 20"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+            />
+          </svg>
+        </button>
+        {open && (
+          <input
+            type="search"
+            id="default-search"
+            className="block  w-full h-1/4 p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-zinc-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-zinc-700 dark:border-zinc-600 dark:placeholder-zinc-400 dark:text-white dark:focus:ring-zinc-500 dark:focus:border-zinc-500"
+            placeholder="Search books..."
+          />
+        )}
       </div>
     </div>
   );
