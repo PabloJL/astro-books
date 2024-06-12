4e848e5b5c741addf476cdea86d58a074a281569
PabloJL <jup_jim_7@hotmail.com>
2024-06-12 11:54:13 -0600

    Profile section added

--- src/components/ProfileToggle.jsx
+++ src/components/ProfileToggle.jsx
@@ -33,9 +33,12 @@ export const ProfileToggle = () => {
         id="user-dropdown"
       >
         <div className="px-4 py-3">
-          <span className="block text-sm text-gray-900 dark:text-white">
+          <a
+            href="/Profile"
+            className="block text-sm text-gray-900 dark:text-white"
+          >
             Bonnie Green
-          </span>
+          </a>
           <span className="block text-sm text-gray-500 truncate dark:text-gray-400">
             name@example.com
           </span>
