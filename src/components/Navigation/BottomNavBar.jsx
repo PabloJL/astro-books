027e9ddd85766ebec6d29cd84b09f38ae3f22af5
PabloJL <jup_jim_7@hotmail.com>
2024-06-13 12:07:03 -0600

    Mobile search icon styles changed

--- src/components/Navigation/BottomNavBar.jsx
+++ src/components/Navigation/BottomNavBar.jsx
@@ -47,8 +47,14 @@ export default function BottomNavBar() {
             fill="currentColor"
             viewBox="0 0 20 18"
           >
-            <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-width="2"
+              d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"
+            />
           </svg>
+
           <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">
             Search
           </span>
