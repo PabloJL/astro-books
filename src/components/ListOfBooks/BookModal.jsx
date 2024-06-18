a2d61f3012e0a776d22b0c9b754a06e4dd40f140
PabloJL <jup_jim_7@hotmail.com>
2024-06-18 12:54:25 -0600

    UI changes

--- src/components/ListOfBooks/BookModal.jsx
+++ src/components/ListOfBooks/BookModal.jsx
@@ -5,22 +5,22 @@ export default function BookModal({ title, img, author, pages, handleClick }) {
     <div
       id="crud-modal"
       aria-hidden="true"
-      className="flex flex-col overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 md:h-[calc(100%-1rem)] max-h-full "
+      className="flex flex-col overflow-y-auto overflow-x-hidden fixed top-2 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 md:h-[calc(100%-1rem)] max-h-full "
     >
       <div className="relative p-4  w-full h-auto flex justify-center">
         <div className="flex flex-col w-full h-full items-center bg-white border border-gray-200 rounded-lg shadow md:max-w-3xl  dark:border-zinc-800 dark:bg-zinc-900 ">
-          <div class="flex items-center justify-between p-4 md:p-5 rounded-t w-full">
-            <h3 class="text-2xl font-semibold text-gray-900 dark:text-white">
+          <div className="flex items-center justify-between p-4 md:p-5 rounded-t w-full">
+            <h3 className="text-2xl font-semibold text-gray-900 dark:text-white">
               Edit Progress
             </h3>
             <button
               onClick={handleClick}
               type="button"
-              class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+              className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
               data-modal-hide="default-modal"
             >
               <svg
-                class="w-3 h-3"
+                className="w-3 h-3"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -28,9 +28,9 @@ export default function BookModal({ title, img, author, pages, handleClick }) {
               >
                 <path
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
                 />
               </svg>
