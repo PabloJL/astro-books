ff1e261e65676dcad58ffd542128c0d3ccb037f1
PabloJL <jup_jim_7@hotmail.com>
2024-06-25 13:45:43 -0600

    Few changes

--- src/data/books.js
+++ src/data/books.js
@@ -35,4 +35,13 @@ export const books = [
     opinion:
       "The novel, set in the waning days of the Old West centers around the relationships between several retired Texas Rangers and their adventures driving a cattle herd from Texas to Montana. The novel contains themes including old age, death, unrequited love, and friendship",
   },
+  {
+    id: "p-hail-mary",
+    title: "Project Hail Mary",
+    author: "Andy Weir",
+    img: "https://m.media-amazon.com/images/I/81zD9kaVW9L._AC_UF894,1000_QL80_.jpg",
+    pages: "614",
+    opinion:
+      "Ryland Grace is the sole survivor on a desperate, last-chance mission—and if he fails, humanity and the earth itself will perish.Except that right now, he doesn’t know that. He can’t even remember his own name, let alone the nature of his assignment or how to complete it. All he knows is that he’s been asleep for a very, very long time. And he’s just been awakened to find himself millions of miles from home, with nothing but two corpses for company.His crewmates dead, his memories fuzzily returning, Ryland realizes that an impossible task now confronts him. Hurtling through space on this tiny ship, it’s up to him to puzzle out an impossible scientific mystery—and conquer an extinction-level threat to our species.",
+  },
 ];
