f5343fcbb89a08bd188a6c543f23e61790233a8d
PabloJL <jup_jim_7@hotmail.com>
2024-07-02 13:30:38 -0600

    Changes

--- src/components/SettingsPage/ProfileSettings.jsx
+++ src/components/SettingsPage/ProfileSettings.jsx
@@ -1,12 +1,12 @@
 export default function ProfileSettings() {
   return (
     <div class="h-full w-fill m-auto  bg-zinc-50 dark:bg-zinc-900 rounded-2xl p-8 text-black dark:text-white font-medium text-base md:text-lg flex flex-col">
-      <a href="#" class="border-b-[1px] pb-6 border-gray-400 dark:border-white">
+      <p href="#" class="border-b-[1px] pb-6 border-gray-400 dark:border-white">
         Account Details
-      </a>
-      <a href="#" class="border-b-[1px] py-6 border-gray-400 dark:border-white">
+      </p>
+      <p href="#" class="border-b-[1px] py-6 border-gray-400 dark:border-white">
         Preferences
-      </a>
+      </p>
       <a href="#" class="border-b-[1px] py-6 border-gray-400 dark:border-white">
         Privacy
       </a>
