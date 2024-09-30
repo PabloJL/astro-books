8beddbaca064fc279bc0b7bcc5ee3ec647a77f10
PabloJL <jup_jim_7@hotmail.com>
2024-09-30 09:17:00 -0600

    Search and api connection working

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,14 @@
+import { searchBooks } from "../services/books";
+import { useState } from "react";
+
 export default function Search() {
+  const [search, setSearch] = useState("");
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    const books = await searchBooks(search);
+    console.log(books);
+  };
   return (
     <div className="w-full h-full p-8 md:p-0">
       <div className="block relative">
@@ -19,13 +29,17 @@ export default function Search() {
             />
           </svg>
         </div>
-        <input
-          type="search"
-          id="default-search"
-          className="block  w-full h-full p-4 ps-10 text-lg text-gray-900 border border-gray-300 rounded-xl bg-zinc-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-zinc-900 dark:border-zinc-900 dark:placeholder-zinc-400 dark:text-white dark:focus:ring-zinc-500 dark:focus:border-zinc-500"
-          placeholder="Search books..."
-        />
+        <form onSubmit={handleSearch}>
+          <input
+            id="default-search"
+            onChange={(e) => setSearch(e.target.value)}
+            value={search}
+            className="block  w-full h-full p-4 ps-10 text-lg text-gray-900 border border-gray-300 rounded-xl bg-zinc-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-zinc-900 dark:border-zinc-900 dark:placeholder-zinc-400 dark:text-white dark:focus:ring-zinc-500 dark:focus:border-zinc-500"
+            placeholder="Search books..."
+          />
+        </form>
       </div>
+      <div></div>
     </div>
   );
 }
--- src/services/books.js
+++ src/services/books.js
@@ -5,3 +5,22 @@ export const getBooks = async () => {
   const books = data;
   return books;
 };
+
+export const searchBooks = async (query) => {
+  const apiKey = "AIzaSyAgNUguzdGrqdqyZ3yvYW6QvGklSk-4kww";
+  const url = `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${apiKey}`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Error fetching books data");
+    }
+    const data = await response.json();
+    console.log("Books Data:", data);
+    return data;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+  const books = data;
+  return books;
+};
