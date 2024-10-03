30dcf75504396ab598c34ac618e985cb4bfc590e
PabloJL <jup_jim_7@hotmail.com>
2024-10-03 09:56:50 -0600

    Search for desktop 3 -now working properly, might need tweaks later on-

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -13,8 +13,8 @@ export default function Search() {
 
   return (
     <div className="w-full h-full p-8 md:p-0">
-      <div className="block relative">
-        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+      <div className="block relative ">
+        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none ">
           <svg
             className="w-4 h-4 text-gray-500 dark:text-gray-400"
             aria-hidden="true"
@@ -31,12 +31,12 @@ export default function Search() {
             />
           </svg>
         </div>
-        <form onSubmit={handleSearch} className="justify-items-end">
+        <form onSubmit={handleSearch}>
           <input
             id="default-search"
             onChange={(e) => setSearch(e.target.value)}
             value={search}
-            className="block w-full h-full  p-4 ps-10 text-lg text-gray-900 border border-gray-300 rounded-xl bg-zinc-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-zinc-900 dark:border-zinc-900 dark:placeholder-zinc-400 dark:text-white dark:focus:ring-zinc-500 dark:focus:border-zinc-500"
+            className="block w-full h-full p-4 ps-10 text-lg text-gray-900 border border-gray-300 rounded-xl bg-zinc-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-zinc-900 dark:border-zinc-900 dark:placeholder-zinc-400 dark:text-white dark:focus:ring-zinc-500 dark:focus:border-zinc-500"
             placeholder="Search books..."
           />
         </form>
@@ -47,14 +47,14 @@ export default function Search() {
         {fetchedBooks.map((book) => (
           <li
             key={book.id}
-            className="text-white w-full  text-center justify-items-center p-4"
+            className="text-white w-full h-full text-center justify-items-center p-4 md:p-8 hover:scale-125"
           >
             <img
               src={book.volumeInfo?.imageLinks?.thumbnail}
               alt={book.volumeInfo?.title}
-              className=" rounded-md object-cover  "
+              className=" rounded-md object-cover md:scale-110"
             />
-            <h1 className=" font-black text-base mt-4">
+            <h1 className=" font-black text-base md:text-lg mt-4">
               {book.volumeInfo?.title || "No title available"}
             </h1>
           </li>
